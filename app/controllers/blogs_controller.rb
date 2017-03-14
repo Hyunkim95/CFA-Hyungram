7613561b5351ec44bb258f5e7a060ef012891f23
Hyun Kim <hkim@live.com.au>
2017-03-14 16:54:24 +1100

    inital commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+    redirect_to blogs_path #redirects to index
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:image, :description) #only permits images and description
+  end
+end
